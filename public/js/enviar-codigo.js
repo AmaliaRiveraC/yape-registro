d24cfbdb8e5b6b038cdc6d1c720d4ba18440d453
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-07-21 10:15:13 +0000

    imprimir temporizador

--- public/js/enviar-codigo.js
+++ public/js/enviar-codigo.js
@@ -1,52 +1,56 @@
 var inputCodigo = document.getElementById('contenedor-codigo');
 var contenedorTemporizador = document.getElementById('contenedor-contador');
 var contador = 21;
-imprimirTemporizador();
 
- 
- 
- 
+
+
+
+
 inputCodigo.addEventListener('keyup',  function(event){
 	event.preventDefault();
 	var codigoIngresaoPorUsuario = inputCodigo.value;
 	var codigo = localStorage.getItem('codigo');
 	var tuTelefono = document.getElementById('telephoNumber');
 	var phone = localStorage.getItem('phone');
- 
+
 	tuTelefono.innerHTML = phone;
 
 	if(codigoIngresaoPorUsuario == codigo){
 		window.location.href = 'crear-usuario.html';
+	} else {
+		limiteDeTiempo(phone);
 	}
-	limiteDeTiempo(phone);
+
 
 });
 
 var limiteDeTiempo = function() {
-	
+
 	setTimeout(function(){
 		postJSONDos(api.url2, {
 			"phone": phone
 		})
-	.then(function(response){
+			.then(function(response){
 			console.log(response);
 			enviarCodigoNuevo(response);
 		})}, 21000);
 };
 
 var imprimirTemporizador = function(){
-	var temporizador = setTimeout(retroceder, 21000);
+	contenedorTemporizador.innerHTML = contador;
+	
+	setTimeout(retroceder, 21000);
+	console.log(setTimeout);
 };
 
 var retroceder = function(){
-	contenedorTemporizador.innerHTML = contador;
 	contador--;
 	console.log(contador);
-	
+
 	if(contador == 0){
 		clearTimeout(temporizador);
 	} 
-	
+
 }
 
 var postJSONDos = function(url) {
@@ -67,11 +71,12 @@ var postJSONDos = function(url) {
 		}
 	});
 };
-	
+
 var enviarCodigoNuevo = function(response){
 	console.log(response);
 	var response = JSON.parse(response);
 	var datos = response.data;
 	var nuevoCodigo = datos.code;
-	
 };
+
+imprimirTemporizador();

bff5559b9b8fe72d08f3696b69e5b785e1789b13
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-07-21 10:52:05 +0000

    peleando con la impresion de mi temporizador

--- public/js/enviar-codigo.js
+++ public/js/enviar-codigo.js
@@ -1,6 +1,8 @@
 var inputCodigo = document.getElementById('contenedor-codigo');
 var contenedorTemporizador = document.getElementById('contenedor-contador');
 var contador = 21;
+var phone = localStorage.getItem('phone');
+var temporizador;
 
 
 
@@ -11,14 +13,13 @@ inputCodigo.addEventListener('keyup',  function(event){
 	var codigoIngresaoPorUsuario = inputCodigo.value;
 	var codigo = localStorage.getItem('codigo');
 	var tuTelefono = document.getElementById('telephoNumber');
-	var phone = localStorage.getItem('phone');
+
 
 	tuTelefono.innerHTML = phone;
-
 	if(codigoIngresaoPorUsuario == codigo){
 		window.location.href = 'crear-usuario.html';
 	} else {
-		limiteDeTiempo(phone);
+		limiteDeTiempo()
 	}
 
 
@@ -26,57 +27,59 @@ inputCodigo.addEventListener('keyup',  function(event){
 
 var limiteDeTiempo = function() {
 
-	setTimeout(function(){
+	setTimeout(imprimirTemporizador, 21000);
+};
+
+var imprimirTemporizador = function(){
+	contenedorTemporizador.innerHTML = contador;
+	contador--;
+
+	temporizador = setTimeout(function(){
+
+		for(var i = 0; i < 1; i--){
+			contador--;
+			contenedorTemporizador.innerHTML = contador;
+		} 
+	}, 21000);
+}
+
+
+
+
+
+	var postJSONDos = function(url) {
+		return new Promise(function(resolve, reject) {
+			var ajax = new XMLHttpRequest();
+			ajax.open("POST", url);
+			ajax.setRequestHeader("Content-Type", "application/json");
+
+			ajax.send(JSON.stringify({
+				"phone": data.phone
+			}));
+			ajax.onreadystatechange = function(){
+				if(ajax.readyState == 4){ 
+					resolve(ajax.responseText)
+				} else {
+					console.log("error");
+				}
+			}
+		});
+	};
+
+	var enviarCodigoNuevo = function(response){
+		console.log(response);
+		var response = JSON.parse(response);
+		var datos = response.data;
+		var nuevoCodigo = datos.code;
+	};
+
+	imprimirTemporizador();
+
+	/*function(){
 		postJSONDos(api.url2, {
 			"phone": phone
 		})
 			.then(function(response){
 			console.log(response);
 			enviarCodigoNuevo(response);
-		})}, 21000);
-};
-
-var imprimirTemporizador = function(){
-	contenedorTemporizador.innerHTML = contador;
-	
-	setTimeout(retroceder, 21000);
-	console.log(setTimeout);
-};
-
-var retroceder = function(){
-	contador--;
-	console.log(contador);
-
-	if(contador == 0){
-		clearTimeout(temporizador);
-	} 
-
-}
-
-var postJSONDos = function(url) {
-	return new Promise(function(resolve, reject) {
-		var ajax = new XMLHttpRequest();
-		ajax.open("POST", url);
-		ajax.setRequestHeader("Content-Type", "application/json");
-
-		ajax.send(JSON.stringify({
-			"phone": data.phone
-		}));
-		ajax.onreadystatechange = function(){
-			if(ajax.readyState == 4){ 
-				resolve(ajax.responseText)
-			} else {
-				console.log("error");
-			}
-		}
-	});
-};
-
-var enviarCodigoNuevo = function(response){
-	console.log(response);
-	var response = JSON.parse(response);
-	var datos = response.data;
-	var nuevoCodigo = datos.code;
-};
-
-imprimirTemporizador();
+		})}*/
